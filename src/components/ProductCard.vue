07af240b0a8d7408e9398c82618ccbd1527004a5
Leonardo Diaz <jldiaz9623@gmail.com>
2025-07-18 16:24:36 -0500

    Diseño del listado de productos

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -5,8 +5,13 @@ defineProps({
 </script>
 
 <template>
-  <router-link to="/">
-    <h2>{{ product.title }}</h2>
-    <p>{{ product.price }}</p>
+  <router-link to="/" class="flex flex-col h-[400px] items-center border-2 border-gray-700 bg-gray-700 hover:bg-purple-900 hover:border-purple-900 transition-colors rounded-lg ">
+    <div class="w-full bg-white rounded-t-lg h-[70%] row-span-3 py-4 px-2">
+      <img :src="product.image" alt="" class="object-contain w-full h-full" />
+    </div>
+    <div class="w-full h-[30%] row-span-2 border-t-2 rounded-b-sm text-white flex flex-col items-center justify-center gap-2 leading-4 px-3">
+      <h2 class="font-semibold text-center">{{ product.title }}</h2>
+      <p class="font-semibold text-xl">$ {{ product.price }}</p>
+    </div>
   </router-link>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,16 @@
 <template>
-  <ProductCard 
-    v-for="product in products" 
-    :key="product.id" 
-    :product="product"
-  />
+  <div class="flex">
+    <div class="w-[20%]">barra</div>
+    <div class="w-full bg-black p-4 grid grid-cols-3 gap-10">
+      <ProductCard 
+        v-for="product in products" 
+        :key="product.id" 
+        :product="product"
+      />
+    </div>
+  </div>
+  
+  
 </template>
 
 <script setup>
